76021bd5de7faffbe9705c737a3118a930b59724
vutienduong <lucifervtd@gmail.com>
2017-05-14 23:39:22 +0900

    Commit 3

---
+++ inv_norm_job.m
@@ -0,0 +1,12 @@
+%-----------------------------------------------------------------------
+% Job configuration created by cfg_util (rev $Rev: 4252 $)
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.spatial.normalise.write.subj.matname = '<UNDEFINED>';
+matlabbatch{1}.spm.spatial.normalise.write.subj.resample = '<UNDEFINED>';
+matlabbatch{1}.spm.spatial.normalise.write.roptions.preserve = 0;
+matlabbatch{1}.spm.spatial.normalise.write.roptions.bb = [NaN NaN NaN
+                                                          NaN NaN NaN];
+matlabbatch{1}.spm.spatial.normalise.write.roptions.vox = [NaN NaN NaN];
+matlabbatch{1}.spm.spatial.normalise.write.roptions.interp = 1;
+matlabbatch{1}.spm.spatial.normalise.write.roptions.wrap = [0 0 0];
+matlabbatch{1}.spm.spatial.normalise.write.roptions.prefix = 'w';
