1b74be4f853399b5c9a16d01ad27706d3dca71c4
vutienduong <lucifervtd@gmail.com>
2017-05-20 17:54:43 +0900

    Change interpolation method

--- inv_norm_job.m
+++ inv_norm_job.m
@@ -7,6 +7,6 @@ matlabbatch{1}.spm.spatial.normalise.write.roptions.preserve = 0;
 matlabbatch{1}.spm.spatial.normalise.write.roptions.bb = [NaN NaN NaN
                                                           NaN NaN NaN];
 matlabbatch{1}.spm.spatial.normalise.write.roptions.vox = [NaN NaN NaN];
-matlabbatch{1}.spm.spatial.normalise.write.roptions.interp = 1;
+matlabbatch{1}.spm.spatial.normalise.write.roptions.interp = 0;
 matlabbatch{1}.spm.spatial.normalise.write.roptions.wrap = [0 0 0];
 matlabbatch{1}.spm.spatial.normalise.write.roptions.prefix = 'w';
