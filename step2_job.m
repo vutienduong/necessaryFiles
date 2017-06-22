8d33a59742875a5322ebf9a9c5daf03b2d2cfb7b
vutienduong <lucifervtd@gmail.com>
2017-06-22 13:44:26 +0700

    nhap

--- step2_job.m
+++ step2_job.m
@@ -8,9 +8,9 @@ matlabbatch{1}.spm.spatial.preproc.output.CSF = [0 0 0];
 matlabbatch{1}.spm.spatial.preproc.output.biascor = 1;
 matlabbatch{1}.spm.spatial.preproc.output.cleanup = 0;
 matlabbatch{1}.spm.spatial.preproc.opts.tpm = {
-                                               'D:\RESEARCH\spm8\tpm\grey.nii'
-                                               'D:\RESEARCH\spm8\tpm\white.nii'
-                                               'D:\RESEARCH\spm8\tpm\csf.nii'
+                                               'D:\duong\spm8\tpm\grey.nii'
+                                               'D:\duong\spm8\tpm\white.nii'
+                                               'D:\duong\spm8\tpm\csf.nii'
                                                };
 matlabbatch{1}.spm.spatial.preproc.opts.ngaus = [2
                                                  2
